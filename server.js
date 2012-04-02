85f34f91efb375f92c30b8473f6e2698cb3c3129
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-02 05:22:53 -0700

    added example folder

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var now = require('./now.js');
 var everyone = now.initialize({apiKey:'abcdefgh'});
-now.ready(function(){
   everyone.now.distributeMessage = function(name, message){
     console.log("CALLING");
     everyone.now.receiveMessage(name, message);
   }
-});
